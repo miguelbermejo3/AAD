144da614174071258ca6e23c6594b23153fe2ffd
miguel <mbermejo8774@ceuandalucia.es>
2023-12-05 17:26:35 +0100

    hasta ejercicio 3

--- EjerciciosFicheros/src/ejercicio3/app/App.java
+++ EjerciciosFicheros/src/ejercicio3/app/App.java
@@ -1,8 +1,23 @@
 package ejercicio3.app;
 
+import ejercicio3.ficheros.CsvSampleService;
+import ejercicio3.ficheros.FicheroException;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		
+		
+		CsvSampleService service= new CsvSampleService();
+		try {
+			service.convertirCsv("c:/clientes.csv","c:/temporal/clientes.txt" );
+			System.out.println("conversión completa");
+		}catch(FicheroException e) {
+			System.out.println("Error : "+e.getMessage());
+		}
+		
+		
 		
 	}
 
--- EjerciciosFicheros/src/ejercicio3/ficheros/CsvSampleService.java
+++ EjerciciosFicheros/src/ejercicio3/ficheros/CsvSampleService.java
@@ -1,7 +1,69 @@
 package ejercicio3.ficheros;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CsvSampleService {
 
-	
+	public void convertirCsv(String ficheroEntrada, String ficheroSalida) throws FicheroException {
+		List<String[]> filas = leerCsv(ficheroEntrada);
+		escribirCsv(filas, ficheroSalida);
+	}
+
+	public List<String[]> leerCsv(String ruta) throws FicheroException {
+		List<String[]> filas = new ArrayList<>();
+		File file = new File(ruta);
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNext()) {
+				String linea = sc.nextLine();
+				String[] campos = linea.split(";");
+				filas.add(campos);
+			}
+			return filas;
+		} catch (IOException e) {
+			throw new FicheroException("error leyendo Csv: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+	public void escribirCsv(List<String[]> filas, String ficheroSalida) throws FicheroException {
+
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(ficheroSalida);
+			for (String[] campos : filas) {
+				StringBuilder nuevaLinea = new StringBuilder();
+				for (int i = 0; i < campos.length; i++) {
+					nuevaLinea.append(campos[i]);
+					if (i < campos.length - 1) {
+						nuevaLinea.append(",");
+					}
+				}
+				writer.write(nuevaLinea.toString() + "\n");
+			}
+
+		} catch (IOException e) {
+			throw new FicheroException("Error escribiendo CSV: " + e.getMessage());
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (Exception ignore) {
+				}
+			}
+
+		}
+
+	}
 
 }
